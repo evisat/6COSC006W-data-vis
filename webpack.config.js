a9350107d15a1bdb9a7a69502937972b49cbc879
evisat <evisaterziu@googlemail.com>
2019-03-29 19:20:53 +0000

    first attempt at including handlebars file

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,41 @@
-var path = require('path')
-var HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+const Handlebars = require('handlebars');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/app.js',
-  output: {
-    filename: 'application.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [ 'style-loader', 'css-loader' ]
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['@babel/preset-env']
-        }
-      }
+    entry: './src/app.js',
+    output: {
+        filename: 'application.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader' ]
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['@babel/preset-env']
+                }
+            },
+            {
+                test: /\.hbs/,
+                loader: "handlebars-loader"
+            }
+        ]
+    },
+    plugins: [
+        new webpack.LoaderOptionsPlugin({
+            options: {
+              handlebarsLoader: {}
+            }
+        }),
+        new HtmlWebpackPlugin({
+            title: '6COSC006W-data-vis',
+            template: 'src/index.hbs'
+        })
     ]
-  },
-  plugins: [new HtmlWebpackPlugin({ template: 'src/index.html' })]
 }
